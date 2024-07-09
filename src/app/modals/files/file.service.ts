5729d7bdb7eabceb5ff45e7e5cef22aa37e8be62
Francisco <caviglia80@gmail.com>
2024-07-09 20:25:14 +0200

    .

--- src/app/modals/files/file.service.ts
+++ src/app/modals/files/file.service.ts
@@ -1,24 +1,29 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FileService {
-  private baseUrl = 'http://localhost:3000/files';  // URL local del servidor NestJS
+  //  private baseUrl = environment.apiUrl;
+  private baseUrl = 'http://localhost:3000/files';
+  private headers = {
+    'bypass-tunnel-reminder': 'xx'
+  };
 
   constructor(private http: HttpClient) { }
 
   getFiles(path: string = ''): Observable<any[]> {
-    return this.http.get<any[]>(`${this.baseUrl}`, { params: { path } });
+    return this.http.get<any[]>(`${this.baseUrl}`, { params: { path }, headers: this.headers });
   }
 
   getFileContent(path: string): Observable<Blob> {
-    return this.http.get(`${this.baseUrl}/content`, { params: { path }, responseType: 'blob' });
+    return this.http.get(`${this.baseUrl}/content`, { params: { path }, responseType: 'blob', headers: this.headers });
   }
 
   downloadFolderAsZip(path: string): Observable<Blob> {
-    return this.http.get(`${this.baseUrl}/download-zip`, { params: { path }, responseType: 'blob' });
+    return this.http.get(`${this.baseUrl}/download-zip`, { params: { path }, responseType: 'blob', headers: this.headers });
   }
 }
